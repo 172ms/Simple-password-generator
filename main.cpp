0355d744f7948951aa0cf6dcce4a3ef45792f740
172ms <84940931+172ms@users.noreply.github.com>
2023-05-06 03:05:29 +0300

    Commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+string generatePssword(int length) {
+    char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}[];:<>,.?/";
+    int size = sizeof(characters) - 1;
+    string password;
+    
+    for (int i = 0; i < length; i++) {
+        password += characters[rand() % size];
+    }
+    
+    return password;
+}
+
+int main() {
+    int lengthPassword;
+    
+    cout << "Hello user, this is a lightweight program that generates a password. (MIN LENGTH = 6, MAX LENGTH = 32)" << endl;
+    cout << "Enter the length of the password: ";
+    cin >> lengthPassword;
+    
+    if (cin.fail() || lengthPassword < 6 || lengthPassword > 32) {
+        cout << "[!] Invalid password length!" << endl;
+        return 1;
+    }
+    
+    string password = generatePssword(lengthPassword);
+    cout << "Your password is: " << password << endl;
+    return 0;
+}
